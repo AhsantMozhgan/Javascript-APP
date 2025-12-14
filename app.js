7f916c79c04e96d01fb6b522322a014dae5a37f5
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-12-14 11:53:19 -0800

    variables-strings-numbers

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+// String
+let firstName = 'Mozhgan'
+let lastName = 'Ahsant'
+// let fullName = 'Mozhgan Ahsant'
+// let fullName = firstName + lastName
+let fullName = firstName + ' ' + lastName
+
+// Number
+let num1 = -12
+let num2 = 20
+let sum = num1 + num2
+
+let res = num1 + num2 + fullName
+let res1 = fullName + num1 + num2
+console.log(sum)        // 8
+console.log(res1)       // Mozhgan Ahsant-1220
