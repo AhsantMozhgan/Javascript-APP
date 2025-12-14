5e006f12080c2f3c6c2e5bc59ccfb50ffc499129
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-12-14 13:40:33 -0800

    template-strings

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-// let sum = function(num1, num2, num3) {
-//     let res = num1 + num2 + num3
-//     return res
-// }
-
-// let showResult = sum(2, 3, 4)
-// console.log(showResult)     // 9
-
-let getUserInfo = function(name = 'Test', id = 2) {
-    return 'Name: ' + name + ' ' + 'ID: ' + id 
+let getUserInfo = function(name, id) {
+    return `Name: ${name} - ID: ${id}`
 }
 
 let showResult = getUserInfo('Mozhan', 22)
