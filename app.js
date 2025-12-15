a467fb635c0dc9ea758694817c284dc70f6e93e0
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-12-15 12:15:58 -0800

    findindex-method

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-const cartItems = ['book1', 'book2', 'book3', 'book4']
+const cartItems = [{
+    title: 'Book1',
+    price: 29
+}, {
+    title: 'Book2',
+    price: 59
+}, {
+    title: 'Book3',
+    price: 79
+}]
 
-console.log(cartItems.indexOf('book3')) //2
-console.log(cartItems.indexOf('Test')) //-1
+// const indexValue = cartItems.findIndex(function(item, index) {
+//     return item.title === 'book2'
+// })
+// console.log(indexValue)     //1
+
+const findProducts = function(cart, productTitle) {
+    const indexValue = cart.findIndex(function(item, index) {
+        // return item.title === productTitle
+        return item.title.toLowerCase() === productTitle
+    })
+    return cart[indexValue]
+}
+const result = findProducts(cartItems, 'book3')
+console.log(result)
