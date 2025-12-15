3076b0ab61caa74b12b65c7ff425e8c131441ce5
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-12-15 11:55:57 -0800

    indexof-method

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 const cartItems = ['book1', 'book2', 'book3', 'book4']
 
-for (let count = 0; count < cartItems.length; count++) {
-    // console.log(cartItems[count])
-    const num = count + 1
-    const productName = cartItems[count]
-    console.log(`${num} - product Name: ${productName}`)
-}
+console.log(cartItems.indexOf('book3')) //2
+console.log(cartItems.indexOf('Test')) //-1
