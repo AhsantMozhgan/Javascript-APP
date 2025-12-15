2eb6ebb64623c02fce7f5a84dc4acb0747e90e1c
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-12-15 10:18:48 -0800

    foreach-method

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const cartItems = ['book1', 'book2', 'book3', 'book4']
-// cartItems.push('book5')
-// cartItems.pop()
-// cartItems.unshift('first Item')     //[ 'first Item', 'book1', 'book2', 'book3', 'book4' ]
-// cartItems.shift()                   //[ 'book1', 'book2', 'book3', 'book4' ]
 
-// cartItems.splice(0, 2)   //[ 'book3', 'book4' ]
-// cartItems.splice(1, 2)      //[ 'book1', 'book4' ]
-// cartItems.splice(1, 2, 'New Item')      //[ 'book1', 'New Item', 'book4' ]
-cartItems.splice(0, 2, 'New Item')      //[ 'New Item', 'book3', 'book4' ]
+cartItems.forEach(function(item) {
+    console.log(item)
+}) 
 
-cartItems[2] = 'New Item'   //[ 'New Item', 'book3', 'New Item' ]      
 
-console.log(cartItems)
+cartItems.forEach(function(item, index) {
+    console.log(item)
+    console.log(index)
+}) 
