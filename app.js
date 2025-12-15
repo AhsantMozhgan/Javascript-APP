37ce318aef66e3ff03e2b043438007d5e3036266
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-12-14 16:02:47 -0800

    challenge-02

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-let fullName = 'MozhganAhsant'
+// This code defines a password validation function.
+// Write a JavaScript function named `isValidPassword` that:
+// - Takes one parameter: `password`.  
+// - Returns `true` only if:
+//   - The password length is greater than 8 characters, and  
+//   - The password does NOT contain the substring `"12345"`.  
+// - Otherwise, it returns `false`.
 
-// console.log(fullName.length)    //13
-// console.log(fullName.toUpperCase())     //MOZHGANAHSANT
 
-let password = 'Mozhgan12345'
-// console.log(password.includes('123456'))    //false
 
-let email = '  mozhy.ahsant.com'
-console.log(email)
-console.log(email.trim())
+let isValidPassword = function(password) {
+    if (password.length > 8 && !password.includes('12345')) {
+        return true
+    } else {
+        return false
+    }  
+}
+
+console.log(isValidPassword('abcd12345'))   //false
+console.log(isValidPassword('abcd12'))      //false
+console.log(isValidPassword('abcdabcd22'))  //true
