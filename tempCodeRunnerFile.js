0fb64551ba9d4da9f2267c7b2edc11b3b1b28b0a
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-12-15 16:25:59 -0800

    setup-web-server

--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-cartItems.push('book5')
-// cartItems.pop()
-// cartItems.unshift('first Item')     //[ 'first Item', 'book1', 'book2', 'book3', 'book4' ]
-// cartItems.shift()                   //[ 'book1', 'book2', 'book3', 'book4' ]
